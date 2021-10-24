7a4256fe849810258ad14f40d6ec7200542b3019
Chester Cueto <chestercueto@Chesters-MacBook-Pro.local>
2021-10-24 08:12:51 +0800

    final project

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xAf24a4291DB7b1D6a5369667E3e481dAaAD2b92d';
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
